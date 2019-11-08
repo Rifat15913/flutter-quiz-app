99f4ca5cbc1d54fae3befadd9c748f326f72c6d7
Rifat <rifat@setuptech.international>
2019-11-08 19:10:07 +0600

    Add app bar

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello World!"),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("My Mature App"),
+        ),
+        body: Text("This is a description"),
+      ),
     );
   }
 }
